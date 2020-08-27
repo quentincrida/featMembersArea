0458b9addeaf1eb1ad5cb134a071756c9e09bae4
quentincrida <qcrida@gmail.com>
2020-08-27 06:54:11 +0100

    various plugins added

--- app/public/wp-content/themes/twentytwenty/functions.php
+++ app/public/wp-content/themes/twentytwenty/functions.php
@@ -753,4 +753,8 @@ function twentytwenty_get_elements_array() {
 	* @param array Array of elements
 	*/
 	return apply_filters( 'twentytwenty_get_elements_array', $elements );
+	
+	// // Disable unused shortcodes
+
+	// add_shortcode( 'pluginshortcode', '__return_false' );
 }
